660d055ada3562912898245af301cbbba0a2fc8e
LC208 <maks.2.03@mail.ru>
2024-04-30 20:46:24 +0800

    adding refund function

--- tinkoffpypayment.py
+++ tinkoffpypayment.py
@@ -36,6 +36,7 @@ class TinkoffPaymentCgi(payment.PaymentCgi):
         fail_form += "</html>\n"
         try:
             obj = terminal.init_deal(str(float(self.payment_params["paymethodamount"])*100), f"external_{self.elid}",self.success_page,self.fail_page)
+            logger.info(str(float(self.payment_params["paymethodamount"])*100))
             redirect_url = obj["PaymentURL"]
         except Exception as err:
             logger.error(err.args)
