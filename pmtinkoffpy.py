92fed57ff66d3a23ce6b21f66866ada0158da373
LC208 <maks.2.03@mail.ru>
2024-04-30 19:20:30 +0800

    adding refund function

--- pmtinkoffpy.py
+++ pmtinkoffpy.py
@@ -77,7 +77,7 @@ class TinkoffPaymentModule(payment.PaymentModule):
     def RF_Validate(self, xml: ET.ElementTree):
         logger.info("validate data in refund form")
         logger.info(ET.tostring(xml.getroot(),encoding="unicode"))
-        ET.dump(xml)
+        ET.dump(ET.Element('doc'))
 
     
 
